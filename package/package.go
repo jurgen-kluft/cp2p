5167b6a223e7438b0808bb4aa80eea2758fe0609
jurgen.kluft <jurgen.kluft@gmail.com>
2026-08-06 18:32:39 +0800

    * cp2p; update package

--- package/package.go
+++ package/package.go
@@ -3,7 +3,7 @@ package cp2p
 import (
 	cbase "github.com/jurgen-kluft/cbase/package"
 	cunittest "github.com/jurgen-kluft/cunittest/package"
-	"github.com/jurgen-kluft/gide/denv"
+	"github.com/jurgen-kluft/go-ide/denv"
 )
 
 const (
